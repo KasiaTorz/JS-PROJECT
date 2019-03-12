266511d29b1ce0481cbebfbadf434dca80155407
KasiaTorz <ktorz@wp.pl>
2019-03-12 19:07:03 +0100

    add proper link

--- case2.js
+++ case2.js
@@ -2,3 +2,5 @@ let firstName = "Dylan";
 let lastName = "Israel";
 
 console.log (`${firstName} ${lastName}`);
+console.log ("My name is" + firstName + lastName);
+console.log (` firstName  + lastName   `.trim()length);
