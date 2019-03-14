99874749d64e501820ba2fed8d9d9c1b058897cb
KasiaTorz <ktorz@wp.pl>
2019-03-14 13:43:17 +0100

    instancefo

--- case4.js
+++ case4.js
@@ -7,4 +7,9 @@ console.log (parseInt(example));
  new Number;
  let a = new Number ('123');
  console.log (a);
- 
+ let b = Number ('123');
+ console.log(b);
+
+ a instanceof Number; /*---true */
+ b instanceof Number; /*--false */
+
