5850fa932b9bfda925514136e5befe88a54aa240
KasiaTorz <ktorz@wp.pl>
2019-03-15 15:41:16 +0100

    true

--- case5.js
+++ case5.js
@@ -3,8 +3,8 @@ let example1 = false;
 console.log (example1); /*false */
 
 let example2 = true; /* true*/ 
-let example3 = null;/* */ 
-let example4 = undefined;/* */ 
+let example3 = null;/*false */ 
+let example4 = undefined;/*false */ 
 let example5 = '';/* */ 
 let example6 = NaN;/* */ 
 let example7 = -5;/* */ 
