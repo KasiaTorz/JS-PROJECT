6dcc0c3b1d36b307180fa6ebcd8e0924cd74d101
KasiaTorz <ktorz@wp.pl>
2019-03-20 15:01:22 +0100

    iterration in array

--- case6.js
+++ case6.js
@@ -5,7 +5,9 @@ console.log(array1.length);/*3 */
 /*array1.push("9");['1','2','3','9'];*/
 
 let box1 = ['Kasia',5,true];
-let box2 = box1;
+let box2 = box1.map((box)=>{
+    return box;
+});
 box2.push(11);
 console.log(box1);
 console.log(box2);
