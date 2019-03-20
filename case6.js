de3bc01ddf99c778960eb6476b44ddd3eed93db2
KasiaTorz <ktorz@wp.pl>
2019-03-20 14:45:33 +0100

    declaration of array

--- case6.js
+++ case6.js
@@ -1,5 +1,7 @@
-let array1 = [1,2,3];
+/*let array1 = [1,2,3];
 console.log(array1.length);/*3 */
-console.log (array1[1]);/*2 */
+/*console.log (array1[1]);/*2 */
 
-array1.push("9");/*['1','2','3','9'] */
+/*array1.push("9");['1','2','3','9'];*/
+
+let box1 = ['Kasia',5,true];
