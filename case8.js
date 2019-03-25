9924c8e9766ed6ce4637f991b81741ac21b22318
KasiaTorz <ktorz@wp.pl>
2019-03-25 17:55:20 +0100

    new obje

--- case8.js
+++ case8.js
@@ -2,8 +2,14 @@ let boxy1 = {
 firstName: 'Dumbo',
 };
 
-let boxy2 = boxy1;
+let boxy2 = Object.assign({},boxy1);
 
  boxy2.lastName ='elefant';
 
 boxy2.lastName
+
+        let ob1 = { a: 10 };
+        let ob2 = { b: 20 };
+        let ob3 = { c: 30 };
+        let new_obj = Object.assign(o1, o2, o3);
+        console.log(new_obj);
